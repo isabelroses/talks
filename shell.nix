1c91da79d5e1870c69285e103506530bc0b7af36
isabel <isabel@isabelroses.com>
2024-03-14 22:49:44 +0000

    feat: add utlity scrip

--- shell.nix
+++ shell.nix
@@ -2,5 +2,12 @@
 pkgs.mkShell {
   buildInputs = with pkgs; [
     nodePackages_latest.pnpm
+    python312Packages.httpserver
+    (pkgs.writeShellApplication {
+      name = "serve";
+      text = ''
+        python3 -m http.server -d ./result/dist/
+      '';
+    })
   ];
 }
