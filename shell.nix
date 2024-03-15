9866183d649ad0fb62e951673cd24473dc1d9f66
isabel <isabel@isabelroses.com>
2024-03-15 11:12:06 +0000

    chore: cleanup shell

--- shell.nix
+++ shell.nix
@@ -2,9 +2,9 @@
 pkgs.mkShell {
   buildInputs = with pkgs; [
     nodePackages_latest.pnpm
-    python312Packages.httpserver
     (pkgs.writeShellApplication {
       name = "serve";
+      runtimeInputs = [python312Packages.httpserver];
       text = ''
         python3 -m http.server -d ./result
       '';
